537e6fe3fa3a5d1f3c8baf0d36563dc46246c5cd
Artemii <artemij.chugreev@gmail.com>
2012-04-26 03:06:48 +0400

    it works!
    протестировал, из нашего фронтенда апи дергается на ура
    код дерьмовый, надо будет рефакторить

--- src/main/java/ru/goodsReview/api/servlet/ViewTagCloudFunctionServlet.java
+++ src/main/java/ru/goodsReview/api/servlet/ViewTagCloudFunctionServlet.java
@@ -1,13 +1,25 @@
 package ru.goodsReview.api.servlet;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import ru.goodsReview.api.util.FileUtil;
+import ru.goodsReview.core.model.Product;
+import ru.goodsReview.core.model.Thesis;
+import ru.goodsReview.storage.controller.ProductDbController;
+import ru.goodsReview.storage.controller.ThesisDbController;
 
+import javax.sql.DataSource;
+import javax.swing.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Artemij Chugreev
@@ -25,17 +37,56 @@ import java.io.IOException;
 public class ViewTagCloudFunctionServlet {
 
     @GET
-    @Path("/script")
+    @Path("/product/{product_id}")
     @Produces(MediaType.TEXT_HTML)
-    public String jsExample() throws JSONException {
+    public String viewScript(@PathParam("product_id") long productId) throws JSONException {
         // Return some cliched textual content
         String page = null;
+        List<Thesis> thesisList = thesisesOnProduct(productId);
+        String jsonThesises = thesisesIntoJSON(thesisList, "some product");
         try {
             page = FileUtil.readFileAsString("viewTagCloudFunction.js");
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        return page;
+        return "var data="+ jsonThesises +";\n" + page;
     }
+    private static String thesisesIntoJSON(List<Thesis> thesisList, String productName){
+        JSONObject thesisMain = new JSONObject();
+        JSONObject thesisObject;
+        JSONArray jsonArray = new JSONArray();
+        for(Thesis thesis : thesisList){
+            thesisObject = new JSONObject();
+            try {
+                thesisObject.put("value", thesis.getContent());
+                thesisObject.put("positivity", thesis.getPositivity());
+                thesisObject.put("importance", thesis.getImportance());
+                thesisObject.put("id", thesis.getId());
+            } catch (JSONException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            jsonArray.put(thesisObject);
+        }
+        try {
+            thesisMain.put("thesises", jsonArray);
+            thesisMain.put("product_name", productName);
+        } catch (JSONException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return thesisMain.toString();
+    }
+
+    private static List<Thesis> thesisesOnProduct(long productId){
+        final FileSystemXmlApplicationContext storageContext = new FileSystemXmlApplicationContext(
+                "src/main/resources/database.xml");
+        DataSource dataSource = (DataSource) storageContext.getBean("dataSource");
+        SimpleJdbcTemplate simpleJdbcTemplate = new SimpleJdbcTemplate(dataSource);
+        ProductDbController productDbController = new ProductDbController(simpleJdbcTemplate);
+        ThesisDbController thesisDbController = new ThesisDbController(simpleJdbcTemplate);
+        Product product = productDbController.getProductById(productId);
+        System.out.println("request thesises on product: #" +productId +" -> "+ product.getName());
+        return thesisDbController.getThesesByProductId(productId);
+    }
+
 
 }
--- viewTagCloudFunction.js
+++ viewTagCloudFunction.js
@@ -1,27 +1,26 @@
-
-    function viewTagCloud(id)
+    function viewTagCloud()
     {
-        var paper = Raphael(100, 100, 1000, 1000);
-        $.get('http://localhost:8080/thesis/'+id, function(thesises){
+        var paper = Raphael(450, 100, 1000, 1000);
         var c = paper.image("http://i.imgur.com/Zbxme.png", 200, 250, 150, 200);
             var alpha=0;
             var radius = 200;
-            var dAlpha = ((360.0/thesises.length)/57.3);
+            var dAlpha = ((360.0/data.thesises.length)/57.3);
             var l = Math.sqrt(2*radius*radius - 2*radius*radius*Math.cos(dAlpha));
             var cx = 175;
             var cy = 175;
             var text_mas=[];
             var coords = [];
-            for(var i=0; i<thesises.length; i++){
+
+            for(var i=0; i<data.thesises.length; i++){
                 cx += l * Math.cos(alpha);
                 cy += l * Math.sin(alpha);
                 console.log(cx + " " + cy);
                 coords[i] = {"cx":cx,"cy":cy};
-                var t = paper.text(cx, cy,thesises[i].value).attr({
+                var t = paper.text(cx, cy,data.thesises[i].value).attr({
                     'fill': Raphael.getRGB("green"),
-                    'opacity':(thesises[i].positivity/6),
-                    'font-size': (thesises[i].importance*100 +15) ,
-                    'font-family': 'arial'
+//                    'opacity':(data.thesises[i].positivity/6),
+                    'font-size': (data.thesises[i].importance*10 +15) ,
+//                    'font-family': 'arial'
                 });
 
                // t.animate({x:20,x:20},2000);
@@ -30,39 +29,6 @@
                 alpha+=dAlpha;
             }
 
-           text_mas[0].animate({x:cx+l * Math.cos(alpha),y:cy+l * Math.sin(alpha)},1000);
-           });
-       /* while(true){
-            cx = 175 + l * Math.cos(alpha);
-            cy = 175 + l * Math.sin(alpha);
-            for(var i=0,t=1;t<text_mas.length;i++,t++){
-                text_mas[i].raphael.animate({'cx':cx,'cy':cy});
-                text_mas[t].raphael.animate({'cx':(cx+ l * Math.cos(alpha)),'cy':(cy+ l * Math.sin(alpha))});
-                cx+=l * Math.cos(alpha);
-                cy+=l * Math.sin(alpha);
-            }
-        }*/
-
-       /* cx = 175 + l * Math.cos(alpha);
-        cy = 175 + l * Math.sin(alpha);
-        while(true){
-            var anim = Raphael.animation({cx:cx,cy:cy},2e3);
-            var anim2 = Raphael.animation({cx:cx+l * Math.cos(alpha),cy:cy+l * Math.sin(alpha)},2e3);
-            for(var i=0,t=1;t<Items.lenght;i++,t++){
-
-              Items[i].str.animate(anim).attr({
-                   'fill': Raphael.getRGB("green"),
-                   'font-size': (Items[i].importance*100 +15) ,
-                   'font-family': 'arial'
-               });
-               Items[t].str.animate(anim2).attr({
-                   'fill': Raphael.getRGB("green"),
-                   'font-size': (Items[i].importance*100 +15) ,
-                   'font-family': 'arial'});
-                cx += l * Math.cos(alpha);
-                cy += l * Math.sin(alpha);
-            }
-        }*/
-
+//           text_mas[0].animate({x:cx+l * Math.cos(alpha),y:cy+l * Math.sin(alpha)},1000);
     }
 
