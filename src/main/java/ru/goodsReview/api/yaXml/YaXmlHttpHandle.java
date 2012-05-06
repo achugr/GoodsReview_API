ea39c4789d523fe89cd1ce1e13c850a58ada7b56
Artemii <dfh>
2012-05-06 23:06:41 +0400

    ya searcher

---
+++ src/main/java/ru/goodsReview/api/yaXml/YaXmlHttpHandle.java
@@ -0,0 +1,26 @@
+package ru.goodsReview.api.yaXml;
+
+import ru.goodsReview.api.util.YaSearcher;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.io.FileNotFoundException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: achugr
+ * Date: 06.05.12
+ * Time: 21:33
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("yaXml")
+public class YaXmlHttpHandle {
+
+    @GET
+    @Path("/{yaXmlRequest}")
+    public int frequency(@PathParam("yaXmlRequest") String request) throws FileNotFoundException {
+        YaSearcher yaSearcher = new YaSearcher();
+        return Integer.parseInt(yaSearcher.sendRequest(request));
+    }
+}
